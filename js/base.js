eca54467e236339ca43db242aedc5d8801820392
Robert Nyman <robert@robertnyman.com>
2013-01-15 16:23:01 +0100

    Added offline support

--- js/base.js
+++ js/base.js
@@ -1,24 +1,29 @@
 // Install app
-var checkIfInstalled = navigator.mozApps.getSelf();
-checkIfInstalled.onsuccess = function () {
-    if (checkIfInstalled.result) {
-    // Already installed
-    }
-    else {
-        var install = document.querySelector("#install"),
-            manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
-        install.parentNode.className = "show-install";
-        install.onclick = function () {
-            var installApp = navigator.mozApps.install(manifestURL);
-            installApp.onsuccess = function(data) {
-                install.style.display = "none";
+if (navigator.mozApps) {
+    var checkIfInstalled = navigator.mozApps.getSelf();
+    checkIfInstalled.onsuccess = function () {
+        if (checkIfInstalled.result) {
+        // Already installed
+        }
+        else {
+            var install = document.querySelector("#install"),
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+            install.parentNode.className = "show-install";
+            install.onclick = function () {
+                var installApp = navigator.mozApps.install(manifestURL);
+                installApp.onsuccess = function(data) {
+                    install.style.display = "none";
+                };
+                installApp.onerror = function() {
+                    alert("Install failed\n\n:" + installApp.error.name);
+                };
             };
-            installApp.onerror = function() {
-                alert("Install failed\n\n:" + installApp.error.name);
-            };
-        };
-    }
-};
+        }
+    };
+}
+else {
+    console.log("Open Web Apps not supported");
+}
 
 // Reload content
 var reload = document.querySelector("#reload");
---
+++ js/offline.js
@@ -0,0 +1,20 @@
+(function () {
+    var appCache = window.applicationCache;
+    if (appCache) {
+        appCache.onupdateready = function () {
+            if (confirm("The app has been updated. Do you want to download the latest files? \nOtherwise they will be updated at the next reload.")) {
+                location.reload(true);
+            }
+        };
+
+        var displayStatus = document.querySelector("#online-status");
+        if (displayStatus) {
+            // Using this, since navigator.onLine is very inconcistent and unreliable
+            appCache.onerror = function() {
+                displayStatus.className = "offline";
+                displayStatus.title = "Offline";
+            };
+            
+        }
+    }
+})();
