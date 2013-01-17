4feeb7613590194527ebe716486df214b93c0116
Robert Nyman <robert@robertnyman.com>
2013-01-17 01:10:41 +0100

    Testing appcache

--- js/offline.js
+++ js/offline.js
@@ -1,6 +1,7 @@
 (function () {
     var appCache = window.applicationCache;
     if (appCache) {
+        window.applicationCache.swapCache();
         appCache.onupdateready = function () {
             if (confirm("The app has been updated. Do you want to download the latest files? \nOtherwise they will be updated at the next reload.")) {
                 location.reload(true);
