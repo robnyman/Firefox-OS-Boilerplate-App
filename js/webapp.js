04698e30636fc2cb30c706b5912c3816773f5c36
Robert Nyman <robert@robertnyman.com>
2013-05-28 10:05:11 +0200

    Fixing image sharing example

--- js/webapp.js
+++ js/webapp.js
@@ -147,16 +147,40 @@
     var shareImage = document.querySelector("#share-image"),
         imgToShare = document.querySelector("#image-to-share");
     if (shareImage && imgToShare) {
+        imgToShare.onload = function () {
+            imgToShare.dataset.loaded = true;
+        };
+
+        imgToShare.onerror = function () {
+            imgToShare.dataset.loaded = false;
+        };
+
         shareImage.onclick = function () {
-            var blob = new Blob([imgToShare], {type: "text/png"});
-            var sharingImage = new MozActivity({
-                name: "share",
-                data: {
-                    type: "image/*",
-                    number: 1,
-                    blobs: [blob]
-                }
-            });
+            if(imgToShare.dataset.loaded === "true") {
+                // Create dummy canvas
+                var blobCanvas = document.createElement("canvas");
+                blobCanvas.width = imgToShare.width;
+                blobCanvas.height = imgToShare.height;
+
+                // Get context and draw image
+                var blobCanvasContext = blobCanvas.getContext("2d");            
+                blobCanvasContext.drawImage(imgToShare, 0, 0);
+
+                // Export to blob and share through a Web Activitiy
+                blobCanvas.toBlob(function (blob) {
+                    var sharingImage = new MozActivity({
+                        name: "share",
+                        data: {
+                            type: "image/*",
+                            number: 1,
+                            blobs: [blob]
+                        }
+                    });
+                });
+            }
+            else {
+                alert("Image failed to load, can't be shared");
+            }
         }
     }
 
