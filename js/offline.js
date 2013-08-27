3f7dfd4aa42b3291d16b942338aed1ea403e2f5d
Zeno Rocha <zno.rocha@gmail.com>
2013-08-27 01:41:43 -0300

    Moving from inline events to addEventListener

--- js/offline.js
+++ js/offline.js
@@ -1,19 +1,19 @@
 (function () {
     var appCache = window.applicationCache;
     if (appCache) {
-        appCache.onupdateready = function () {
+        appCache.addEventListener("updateready", function() {
             if (confirm("The app has been updated. Do you want to download the latest files? \nOtherwise they will be updated at the next reload.")) {
                 location.reload(true);
             }
-        };
+        });
 
         var displayStatus = document.querySelector("#online-status");
         if (displayStatus) {
             // Using this, since navigator.onLine is very inconcistent and unreliable
-            appCache.onerror = function() {
+            appCache.addEventListener("error", function() {
                 displayStatus.className = "offline";
                 displayStatus.title = "Offline";
-            };
+            });
         }
     }
 })();
