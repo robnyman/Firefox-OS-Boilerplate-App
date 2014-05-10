a499f5b34e300f53b43b3a76e838eeb3031d32f8
root <root@siduction>
2014-05-10 18:49:58 +0200

    change the path for the manifest in base.js

--- js/base.js
+++ js/base.js
@@ -13,7 +13,7 @@ if (navigator.mozApps) {
         }
         else {
             var install = document.querySelector("#install"),
-                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest-hosted.webapp";
             install.className = "show-install";
             install.onclick = function () {
                 var installApp = navigator.mozApps.install(manifestURL);
